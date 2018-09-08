70955e8012ea24dcf2d9ad3bbf8a2f67c90fdeb2
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-09-08 20:31:05 +0200

    Implementate le funzioni di inserimento dei due numeri

---
+++ matematica/src/get_numbers/get_numbers.c
@@ -0,0 +1,27 @@
+/*
+ * get_numbers.c
+ *
+ *  Created on: 08 set 2018
+ *      Author: gabriele
+ */
+
+#include "get_numbers.h"
+
+static const char* SPEC_INT = "&d";
+static const char* INSERT_SECOND_NUMBER = "Inserisci il secondo numero>";
+static const char* INSERT_FIRST_NUMBER = "Inserisci il primo numero>";
+
+int get_first_number(){
+	int number;
+	printf(INSERT_FIRST_NUMBER);
+	scanf(SPEC_INT, &number);
+	return number;
+}
+
+int get_second_number(){
+	int number;
+	printf(INSERT_SECOND_NUMBER);
+	scanf(SPEC_INT, &number);
+	return number;
+}
+
---
+++ matematica/src/get_numbers/get_numbers.h
@@ -0,0 +1,25 @@
+/*
+ * get_numbers.h
+ *
+ *  Created on: 08 set 2018
+ *      Author: gabriele
+ */
+
+#ifndef GET_NUMBERS_GET_NUMBERS_H_
+#define GET_NUMBERS_GET_NUMBERS_H_
+
+#include <stdio.h>
+
+/**
+ * Inserimento da tastiera del primo numero
+ * @return
+ */
+int get_first_number();
+
+/**
+ * Inserimento da tastiera del primo numero
+ * @return
+ */
+int get_second_number();
+
+#endif /* GET_NUMBERS_GET_NUMBERS_H_ */
